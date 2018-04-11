2e2837a331546fc6998ed2101a17f2c3bd886f04
Mike <gorczynski.mike@gmail.com>
2018-04-11 12:40:24 +0200

    SimpleUser class modified, modified SimpleUser tests

--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -3,13 +3,19 @@ package com.kodilla.testing.user;
 public class SimpleUser {
 
     private String userName;
+    private String realName;
 
-    public SimpleUser(String userName) {
+    public SimpleUser(String userName, String realName) {
         this.userName = userName;
+        this.realName = realName;
     }
 
     public String getUserName() {
         return this.userName;
+    }
+
+    public String getRealName() {
+        return this.realName;
     }
 
 }
