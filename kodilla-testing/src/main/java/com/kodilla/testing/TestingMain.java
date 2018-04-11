5aad24cdb4d6041c3a5d19cce8736ade1d9b7970
Mike <gorczynski.mike@gmail.com>
2018-04-11 12:07:30 +0200

    Added class Calculator and tests of new functionality in TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("SimpleUser test OK");
+        } else {
+            System.err.println("SimpleUser test Error");
+        }
+
+        // com.kodilla.testing.calculator.Calculator tests
+        Calculator calculator = new Calculator();
+
+        // Test 1 Calculator.add()
+        int calculatorAddTestResult = calculator.add(4,7);
+        if(calculatorAddTestResult == 11) {
+            System.out.println("Calculator.add() test OK");
+        } else {
+            System.err.println("Calculator.add() test ERROR");
+        }
+
+        // Test 2 Calculator.subtract()
+        int calculatorSubtractTestResult = calculator.subtract(4,7);
+        if(calculatorSubtractTestResult == -3) {
+            System.out.println("Calculator.subtract() test OK");
+        } else {
+            System.err.println("Calculator.subtract() test ERROR");
+        }
     }
 
 }
