89d0d2bc36e28a2123dab7402bf774f4311746a5
Mike <gorczynski.mike@gmail.com>
2018-05-05 15:52:19 +0200

    some minor changes

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/FoodOrderRejectionReason.java
@@ -0,0 +1,28 @@
+package com.kodilla.good.patterns.challenges.food2door;
+
+public enum FoodOrderRejectionReason {
+    NULL(0),
+    ALL_OK_ORDER_ACCEPTED(1),
+    NO_SUCH_PRODUCT(101),
+    NOT_ENOUGH_IN_STOCK(102),
+    NOT_LUXURY_ENOUGH(201),
+    NOT_HEALTHY_ENOUGH(202),
+    NOT_GLUTEN_FREE(203);
+
+    private final int rejectionCode;
+
+    FoodOrderRejectionReason(int rejectionCode) {
+        this.rejectionCode = rejectionCode;
+    }
+
+    public int getRejectionCode() {
+        return this.rejectionCode;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodOrderRejectionReason{" +
+                "rejectionCode=" + rejectionCode +
+                '}';
+    }
+}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/FoodSupplierFeedbackDto.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/FoodSupplierFeedbackDto.java
@@ -4,10 +4,18 @@ public class FoodSupplierFeedbackDto {
 
     private final boolean orderProcessedSuccessfully;
     private final String message;
+    private final FoodOrderRejectionReason rejectionReason;
+
+    public FoodSupplierFeedbackDto(boolean orderProcessedSuccessfully, String message, FoodOrderRejectionReason rejectionReason) {
+        this.orderProcessedSuccessfully = orderProcessedSuccessfully;
+        this.message = message;
+        this.rejectionReason = rejectionReason;
+    }
 
     public FoodSupplierFeedbackDto(boolean orderProcessedSuccessfully, String message) {
         this.orderProcessedSuccessfully = orderProcessedSuccessfully;
         this.message = message;
+        this.rejectionReason = FoodOrderRejectionReason.NULL;
     }
 
     public boolean isOrderProcessedSuccessfully() {
