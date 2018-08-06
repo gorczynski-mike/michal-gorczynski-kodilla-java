ea194923be3587d333b2b4d0f3952cd530295a1e
Mike <gorczynski.mike@gmail.com>
2018-08-06 21:22:25 +0200

    added adapter design pattern example

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/librarya/Classifier.java
@@ -0,0 +1,9 @@
+package com.kodilla.patterns2.adapter.bookclassifier.librarya;
+
+import java.util.Set;
+
+public interface Classifier {
+
+    int publicationYearMedian(Set<Book> bookSet);
+
+}
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class MedianAdapterTestSuite {
+
+    @Test
+    public void publicationYearMedian() {
+        //Given
+        Set<Book> books = new HashSet<>();
+        books.add(new Book("author1", "title1", 2003, "signature1"));
+        books.add(new Book("author1", "title1", 2009, "signature1"));
+        books.add(new Book("author1", "title1", 2000, "signature1"));
+        books.add(new Book("author1", "title1", 1970, "signature1"));
+        books.add(new Book("author1", "title1", 1999, "signature1"));
+        //When
+        MedianAdapter medianAdapter = new MedianAdapter();
+        int bookPublicationYearMedian = medianAdapter.publicationYearMedian(books);
+        //Then
+        assertEquals(2000, bookPublicationYearMedian);
+    }
+}
